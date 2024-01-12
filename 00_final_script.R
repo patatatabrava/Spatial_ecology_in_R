3757d1d314a6e548982922e846ea437665c26444
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-12 15:48:19 +0100

    Update 00 final script.R

--- 00_final_script.R
+++ 00_final_script.R
@@ -1,3 +1,668 @@
+# Final script including all the different scripts during lectures
+
+#--------------------
+
+# Summary:
+# 01 Beginning
+# 02.1 Population density
+# 02.2 Population distribution
+# 03.1 Community multivariate analysis
+# 03.2 Community overlap
+# 04 Remote sensing data visualisation
+# 05 Spectral indices
+# 06 Time series
+# 07 External data
+# 08 Copernicus data
+# 09 Classification
+# 10 Variability
+# 11 Principal Component Abalysis
+
+#--------------------
+
+# 01 Beginning
+
+# Here I can write anything I want!!! 42 is the meaning of life univcerse and all!
+
+# R as a calculator
+2 + 3
+
+# Assign to an object
+zima <- 2 + 3
+zima
+
+duccio <- 5 + 3
+duccio
+
+final <- zima * duccio
+final
+
+final^2
+
+# array
+sophi <- c(10, 20, 30, 50, 70) # microplastics 
+# fcuntions have parentheses and inside them there are arguments
+
+paula <- c(100, 500, 600, 1000, 2000) # people
+
+plot(paula, sophi)
+
+plot(paula, sophi, xlab="number of people", ylab="microplastics")
+
+people <- paula
+microplastics <- sophi
+
+plot(people, microplastics)
+plot(people, microplastics, pch=19)
+# https://www.google.com/search?client=ubuntu-sn&hs=yV6&sca_esv=570352775&channel=fs&sxsrf=AM9HkKknoSOcu32qjoErsqX4O1ILBOJX4w:1696347741672&q=point+symbols+in+R&tbm=isch&source=lnms&sa=X&ved=2ahUKEwia9brkm9qBAxVrQvEDHbEYDuMQ0pQJegQIChAB&biw=1760&bih=887&dpr=1.09#imgrc=lUw3nrgRKV8ynM
+
+plot(people, microplastics, pch=19, cex=2)
+plot(people, microplastics, pch=19, cex=2, col="blue")
+
+#--------------------
+
+# 02.1 Population density
+
+# Code related to population ecology
+
+# A package is needed for point pattern analysis
+install.packages("spatstat")
+library(spatstat)
+
+# let's use the bei data:
+# data description:
+# https://CRAN.R-project.org/package=spatstat
+
+bei
+
+# plotting the data
+plot(bei)
+
+# changing dimension - cex
+plot(bei, cex=.2)
+
+# changing the symbol - pch
+plot(bei, cex=.2, pch=19)
+
+# additional datasets
+bei.extra
+plot(bei.extra)
+
+# let's use only part of the dataset: elev
+plot(bei.extra$elev)
+elevation <- bei.extra$elev
+plot(elevation)
+
+# second method to select elements
+elevation2 <- bei.extra[[1]]
+plot(elevation2)
+
+# passing from points to a countinuous surface
+densitymap <- density(bei)
+plot(densitymap)
+points(bei, cex=.2)
+
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)
+plot(densitymap, col=cl)
+
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4)
+plot(densitymap, col=cl)
+
+clnew <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+plot(densitymap, col=clnew)
+
+plot(bei.extra)
+
+elev <- bei.extra[[1]] # bei.extra$elev
+plot(elev)
+
+# multiframe
+par(mfrow=c(1,2))
+plot(densitymap)
+plot(elev)
+
+par(mfrow=c(2,1))
+plot(densitymap)
+plot(elev)
+
+par(mfrow=c(1,3))
+plot(bei)
+plot(densitymap)
+plot(elev)
+
+#--------------------
+
+# Why populations disperse over the landscape in a certain manner?
+
+library(sdm)
+library(terra)
+
+file <- system.file("external/species.shp", package="sdm")
+
+rana <- vect(file)
+rana$Occurrence
+
+plot(rana)
+
+# Selecting presences
+pres <- rana[rana$Occurrence==1,]
+plot(pres)
+
+# exercise: select absence and call them abse
+abse <- rana[rana$Occurrence==0,]
+plot(abse)
+
+# exrecise: plot presences and absences, one beside the other
+par(mfrow=c(1,2))
+plot(pres)
+plot(abse)
+
+# your new friend in case of graphical nulling:
+dev.off()
+
+# exercise: plot pres and abse altogether with two different colours
+plot(pres, col="dark blue")
+points(abse, col="light blue")
+
+# predictors: environmental variables
+# file <- system.file("external/species.shp", package="sdm")
+# rana <- vect(file)
+
+# elevation predictor
+elev <- system.file("external/elevation.asc", package="sdm") 
+elevmap <- rast(elev) # from terra package
+plot(elevmap)
+points(pres, cex=.5)
+
+# temperature predictor
+temp <- system.file("external/temperature.asc", package="sdm") 
+tempmap <- rast(temp) # from terra package
+plot(tempmap)
+points(pres, cex=.5)
+
+# exrcise: do the same with vegetation cover
+vege <- system.file("external/vegetation.asc", package="sdm") 
+vegemap <- rast(vege) # from terra package
+plot(vegemap)
+points(pres, cex=.5)
+
+# exrcise: do the same with vegetation cover
+prec <- system.file("external/precipitation.asc", package="sdm") 
+precmap <- rast(prec) # from terra package
+plot(precmap)
+points(pres, cex=.5)
+
+# final multiframe
+
+par(mfrow=c(2,2))
+
+# elev
+plot(elevmap)
+points(pres, cex=.5)
+
+# temp
+plot(tempmap)
+points(pres, cex=.5)
+
+# vege
+plot(vegemap)
+points(pres, cex=.5)
+
+# prec
+plot(precmap)
+points(pres, cex=.5)
+
+#--------------------
+
+# 03.1 Community multivariate analysis
+
+library(vegan)
+
+data(dune)
+head(dune)
+
+ord <- decorana(dune)
+
+ldc1 =  3.7004 
+ldc2 =  3.1166 
+ldc3 = 1.30055
+ldc4 = 1.47888
+
+total = ldc1 + ldc2 + ldc3 + ldc4
+
+pldc1 = ldc1 * 100 / total
+pldc2 = ldc2 * 100 / total
+pldc3 = ldc3 * 100 / total
+pldc4 = ldc4 * 100 / total
+
+pldc1
+pldc2
+
+pldc1 + pldc2
+
+plot(ord) 
+
+#--------------------
+
+# 03.2 Comjmunity overlap
+
+# relation among species in time
+
+library(overlap)
+
+data(kerinci)
+summary(kerinci)
+
+kerinci$timeRad <- kerinci$Time * 2 * pi
+
+tiger <- kerinci[kerinci$Sps=="tiger",]
+
+timetig <- tiger$timeRad
+densityPlot(timetig, rug=TRUE)
+
+# exercise: select only the data on macaque individuals
+macaque <- kerinci[kerinci$Sps=="macaque",]
+head(macaque)
+
+timemac <- macaque$timeRad
+densityPlot(timemac, rug=TRUE)
+
+overlapPlot(timetig, timemac)
+
+#--------------------
+
+# 04 Remote sensing data visualisation
+
+# RS data
+
+library(devtools) # packages in R are also called libraries
+
+# install the imageRy package from GitHub
+install_github("ducciorocchini/imageRy")  # from devtools
+
+library(imageRy)
+library(terra)
+# in case you have not terra
+# install.packages("terra")
+
+# list the data
+im.list()
+
+b2 <- im.import("sentinel.dolomites.b2.tif") 
+
+cl <- colorRampPalette(c("black", "grey", "light grey")) (100)
+plot(b2, col=cl)
+
+# import the green band from Sentinel-2 (band 3)
+b3 <- im.import("sentinel.dolomites.b3.tif") 
+plot(b3, col=cl)
+
+# import the red band from Sentinel-2 (band 4)
+b4 <- im.import("sentinel.dolomites.b4.tif") 
+plot(b4, col=cl)
+
+# import the NIR band from Sentinel-2 (band 8)
+b8 <- im.import("sentinel.dolomites.b8.tif") 
+plot(b8, col=cl)
+
+# multiframe
+par(mfrow=c(2,2))
+plot(b2, col=cl)
+plot(b3, col=cl)
+plot(b4, col=cl)
+plot(b8, col=cl)
+
+# stack images
+stacksent <- c(b2, b3, b4, b8)
+dev.off() # it closes devices
+plot(stacksent, col=cl)
+
+plot(stacksent[[4]], col=cl)
+
+# Exercise: plot in a multiframe the bands with different color ramps
+par(mfrow=c(2,2))
+
+clb <- colorRampPalette(c("dark blue", "blue", "light blue")) (100)
+plot(b2, col=clb)
+
+clg <- colorRampPalette(c("dark green", "green", "light green")) (100)
+plot(b3, col=clg)
+
+clr <- colorRampPalette(c("dark red", "red", "pink")) (100)
+plot(b4, col=clr)
+
+cln <- colorRampPalette(c("brown", "orange", "yellow")) (100)
+plot(b8, col=cln)
+
+# RGB space
+# stacksent: 
+# band2 blue element 1, stacksent[[1]] 
+# band3 green element 2, stacksent[[2]]
+# band4 red element 3, stacksent[[3]]
+# band8 nir element 4, stacksent[[4]]
+im.plotRGB(stacksent, r=3, g=2, b=1)
+im.plotRGB(stacksent, r=4, g=3, b=2)
+im.plotRGB(stacksent, r=3, g=4, b=2)
+im.plotRGB(stacksent, r=3, g=2, b=4)
+
+pairs(stacksent)
+
+#--------------------
+
+# 05 Spectral indices
+
+# vegetation indices
+
+library(imageRy)
+library(terra)
+
+im.list()
+
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")   
+# bands: 1=NIR, 2=RED, 3=GREEN
+im.plotRGB(m1992, r=1, g=2, b=3)
+im.plotRGB(m1992, 1, 2, 3)
+im.plotRGB(m1992, r=2, g=1, b=3)
+im.plotRGB(m1992, r=2, g=3, b=1)
+
+# import the recent image
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+# build a multiframe with 1992 and 2006 images
+par(mfrow=c(1,2))
+im.plotRGB(m1992, r=2, g=3, b=1)
+im.plotRGB(m2006, r=2, g=3, b=1)
+
+# DVI = NIR - RED
+# bands: 1=NIR, 2=RED, 3=GREEN
+
+dvi1992 = m1992[[1]] - m1992[[2]]
+plot(dvi1992)
+
+cl <- colorRampPalette(c("darkblue", "yellow", "red", "black")) (100)
+plot(dvi1992, col=cl)
+
+# exercise: calculate dvi of 2006
+dvi2006 = m2006[[1]] - m2006[[2]]
+plot(dvi2006, col=cl)
+
+# NDVI
+ndvi1992 = (m1992[[1]] - m1992[[2]]) / (m1992[[1]] + m1992[[2]])
+ndvi1992 = dvi1992 / (m1992[[1]] + m1992[[2]])
+plot(ndvi1992, col=cl)
+
+# NDVI
+ndvi2006 = dvi2006 / (m2006[[1]] + m2006[[2]])
+plot(ndvi2006, col=cl)
+
+# par
+par(mfrow=c(1,2))
+plot(ndvi1992, col=cl)
+plot(ndvi2006, col=cl)
+
+clvir <- colorRampPalette(c("violet", "dark blue", "blue", "green", "yellow"))(100) # specifying a color scheme
+par(mfrow=c(1,2))
+plot(ndvi1992, col=clvir)
+plot(ndvi2006, col=clvir)
+
+# speediing up calculation
+ndvi2006a <- im.ndvi(m2006, 1, 2)
+plot(ndvi2006a, col=cl)
+
+#--------------------
+
+# 06 Time series
+
+# time series analysis
+
+library(imageRy)
+library(terra)
+
+im.list()
+
+# import the data
+EN01 <- im.import("EN_01.png")
+EN13 <- im.import("EN_13.png")
+
+par(mfrow=c(2,1))
+im.plotRGB.auto(EN01)
+im.plotRGB.auto(EN13)
+
+# using the first element (band) of images
+dif = EN01[[1]] - EN13[[1]]
+
+# palette
+cldif <- colorRampPalette(c("blue", "white", "red")) (100)
+plot(dif, col=cldif)
+
+
+### New example: temperature in Greenland
+
+g2000 <- im.import("greenland.2000.tif")
+clg <- colorRampPalette(c("black", "blue", "white", "red")) (100)
+plot(g2000, col=clg)
+
+g2005 <- im.import("greenland.2005.tif")
+g2010 <- im.import("greenland.2010.tif")
+g2015 <- im.import("greenland.2015.tif")
+
+plot(g2015, col=clg)
+
+par(mfrow=c(1,2))
+plot(g2000, col=clg)
+plot(g2015, col=clg)
+
+# stacking the data
+stackg <- c(g2000, g2005, g2010, g2015)
+plot(stackg, col=clg)
+
+# Exercise: make the differencxe between the first and the final elemnts of the stack
+difg <- stackg[[1]] - stackg[[4]]
+# difg <- g2000 - g2015
+plot(difg, col=cldif)
+
+# Exercise: make a RGB plot using different years
+im.plotRGB(stackg, r=1, g=2, b=3)
+
+#--------------------
+
+# 07 External data
+
+# External data
+
+library(terra)
+
+# set the working directory based on your path:
+# setwd("youtpath")
+# W***** users: C:\\path\Downloads -> C://path/Downloads
+# My own:
+setwd("~/Downloads")
+naja <- rast("najafiraq_etm_2003140_lrg.jpg")  # like in im.import()
+
+plotRGB(naja, r=1, g=2, b=3) # im-plotRGB
+
+# Exercise: Download the second image from the same site and import it in R
+najaaug <- rast("najafiraq_oli_2023219_lrg.jpg")
+plotRGB(najaaug, r=1, g=2, b=3)
+
+par(mfrow=c(2,1))
+plotRGB(naja, r=1, g=2, b=3) # im-plotRGB
+plotRGB(najaaug, r=1, g=2, b=3)
+
+# multitemporal change detection
+najadif = naja[[1]] - najaaug[[1]] 
+cl <- colorRampPalette(c("brown", "grey", "orange")) (100)
+plot(najadif, col=cl)
+
+# Download your own preferred image:
+typhoon <- rast("mawar_vir2_2023144_lrg.jpg")
+
+plotRGB(typhoon, r=1, g=2, b=3)
+plotRGB(typhoon, r=2, g=1, b=3)
+plotRGB(typhoon, r=3, g=2, b=1)
+
+# The Mato Grosso image can be downloaded directly from EO-NASA:
+
+mato <- rast("matogrosso_l5_1992219_lrg.jpg")
+plotRGB(mato, r=1, g=2, b=3) 
+plotRGB(mato, r=2, g=1, b=3) 
+
+#--------------------
+
+# 08 Copernicus data
+
+# https://land.copernicus.vgt.vito.be/PDF/portal/Application.html
+
+library(ncdf4)
+library(terra)
+
+# install.packages("name_of_the_package_here")
+
+setwd("~/Downloads") # in W*****s \ means /
+
+soilm2023 <- rast("c_gls_SSM1km_202311250000_CEURO_S1CSAR_V1.2.1.nc")
+plot(soilm2023)
+
+# there are two elements, let's use the first one!
+plot(soilm2023[[1]])
+
+cl <- colorRampPalette(c("red", "orange", "yellow")) (100)
+plot(soilm2023[[1]], col=cl)
+
+ext <- c(22, 26, 55, 57) # minlong, maxlong, minlat, maxlat
+soilm2023c <- crop(soilm2023, ext)
+
+plot(soilm2023c[[1]], col=cl)
+
+# new image
+soilm2023_24 <- rast("c_gls_SSM1km_202311240000_CEURO_S1CSAR_V1.2.1.nc")
+plot(soilm2023_24)
+soilm2023_24c <- crop(soilm2023_24, ext)
+plot(soilm2023_24c[[1]], col=cl)
+
+#--------------------
+
+# 09 Classification
+
+# Classifying satellite images and estimate the amount of change
+
+library(terra)
+library(imageRy)
+library(ggplot2)
+library(patchwork)
+
+im.list()
+
+# https://www.esa.int/ESA_Multimedia/Images/2020/07/Solar_Orbiter_s_first_views_of_the_Sun6
+# additional images: https://webbtelescope.org/contents/media/videos/1102-Video?Tag=Nebulas&page=1
+
+sun <- im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+sunc <- im.classify(sun, num_clusters=3)
+
+# classify satellite data
+
+im.list()
+
+m1992 <- im.import("matogrosso_l5_1992219_lrg.jpg")
+m2006 <- im.import("matogrosso_ast_2006209_lrg.jpg")
+  
+m1992c <- im.classify(m1992, num_clusters=2)                    
+plot(m1992c)
+# classes: forest=1; human=2
+
+m2006c <- im.classify(m2006, num_clusters=2)
+plot(m2006c)
+# classes: forest=1; human=2
+
+par(mfrow=c(1,2))
+plot(m1992c[[1]])
+plot(m2006c[[1]])
+
+f1992 <- freq(m1992c)
+f1992
+tot1992 <- ncell(m1992c)
+# percentage
+p1992 <- f1992 * 100 / tot1992 
+p1992
+# forest: 83%; human: 17%
+
+# percentage of 2006
+f2006 <- freq(m2006c)
+f2006
+tot2006 <- ncell(m2006c)
+# percentage
+p2006 <- f2006 * 100 / tot2006 
+p2006
+# forest: 45%; human: 55%
+
+# building the final table
+class <- c("forest", "human")
+y1992 <- c(83, 17)
+y2006 <- c(45, 55) 
+
+tabout <- data.frame(class, y1992, y2006)
+tabout
+
+# final output
+p1 <- ggplot(tabout, aes(x=class, y=y1992, color=class)) + geom_bar(stat="identity", fill="white")
+p2 <- ggplot(tabout, aes(x=class, y=y2006, color=class)) + geom_bar(stat="identity", fill="white")
+p1 + p2
+
+# final output, rescaled
+p1 <- ggplot(tabout, aes(x=class, y=y1992, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p2 <- ggplot(tabout, aes(x=class, y=y2006, color=class)) + geom_bar(stat="identity", fill="white") + ylim(c(0,100))
+p1 + p2
+
+#--------------------
+
+# 10 Variability
+
+# measurement of RS based variability
+
+library(imageRy)
+library(terra)
+library(viridis)
+
+im.list()
+
+sent <- im.import("sentinel.png")
+
+# band 1 = NIR
+# band 2 = red
+# band 3 = green
+
+im.plotRGB(sent, r=1, g=2, b=3)
+im.plotRGB(sent, r=2, g=1, b=3)
+
+nir <- sent[[1]]
+plot(nir)
+
+# moving window
+# focal
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+plot(sd3)
+
+viridisc <- colorRampPalette(viridis(7))(255)
+plot(sd3, col=viridisc)
+
+# Exercise: calculate variability in a 7x7 pixels moving window
+sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
+plot(sd7, col=viridisc)
+
+# Exercise 2: plot via par(mfrow()) the 3x3 and the 7x7 standard deviation
+par(mfrow=c(1,2))
+plot(sd3, col=viridisc)
+plot(sd7, col=viridisc)
+
+# original image plus the 7x7 sd
+im.plotRGB(sent, r=2, g=1, b=3)
+plot(sd7, col=viridisc)
+
+#--------------------
+
+# 11 Principal Component Abalysis
+
 library(imageRy)
 library(terra)
 library(viridis)
@@ -35,3 +700,5 @@ plot(pc1sd7, col=viridisc)
 sdstack <- c(sd3, sd7, pc1sd3, pc1sd7)
 names(sdstack) <- c("sd3", "sd7", "pc1sd3", "pc1sd7")
 plot(sdstack, col=viridisc)
+
+#--------------------
