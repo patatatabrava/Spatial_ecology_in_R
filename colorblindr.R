d71cedd4f81e13904e580737d42fa80245bdfef5
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-12 17:00:46 +0100

    Update colorblindr.R

--- colorblindr.R
+++ colorblindr.R
@@ -6,9 +6,19 @@ library(ggplot2)
 iris
 head(iris) # shows the iris dataset
 
-fig <- ggplot(iris, aes(Sepal.Length, fill = Species)) + geom_density(alpha = 0.7)
-fig
+# Comparing the plot of the sepal length repartion of the flowers seen by a person with standard vision with the same plot seen by a colorblind person
 
-cvd_grid(fig) # "cvd" means "color vision deficiency"
+fig <- ggplot(iris, aes(Sepal.Length, fill = Species)) + geom_density(alpha = 0.7)
+fig # shows the figure
+
+cvd_grid(fig) # "cvd" means "color vision deficiency". This function shows the way a colorblind person sees the figure it takes as an argument
 
 names(iris) # gives all the variables in the dataset iris
+
+# Comparing the plot of the sepal width repartion of the flowers seen by a person with standard vision with the same plot seen by a colorblind person
+
+fig <- ggplot(iris, aes(Sepal.Width, fill = Species)) + geom_density(alpha = 0.7)
+fig
+  
+cvd_grid(fig)
+  
