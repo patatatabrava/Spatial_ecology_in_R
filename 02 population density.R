d6c013c88185628271597527e16d1f6bdf493069
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-14 19:18:13 +0100

    Update 02 population density.R

--- 02 population density.R
+++ 02 population density.R
@@ -2,47 +2,40 @@
 install.packages("spatstat") # this line is for installing the spatstat library into R
 library(spatstat) # that one is for calling it for this paticular script
 
-# We will use the bei dataset from the spatstat library.
-bei
+# We will use the bei dataset from the spatstat library. Description: "A point pattern giving the locations of 3605 trees in a tropical rain forest. Accompanied by covariate data giving the elevation (altitude) and slope of elevation in the study region." This description is given as "hover" text when you type "bei" in the console in R studio.
 
-plot(bei, cex=.2, pch=19) # plots bei as a set of full points of size .2
+bei # loads the dataset
+plot(bei, cex=.2, pch=19) # plots bei as a set of filled up points of size .2
 
-############################################## Stopped working here!!!!!!!!!!!!!!!!!!!
-
-# additional datasets
-bei.extra
+bei.extra # loads the extra datasets attached to bei
 plot(bei.extra)
 
-# let's use only part of the dataset: elev
-plot(bei.extra$elev)
+plot(bei.extra$elev) # the '$' selects the elev part of the extra datasets
 elevation <- bei.extra$elev
 plot(elevation)
 
-# second method to select elements
-elevation2 <- bei.extra[[1]]
+# Second method to select elements:
+elevation2 <- bei.extra[[1]] # the double parentheses are because bei.extra is a 2D dataset
 plot(elevation2)
 
-# passing from points to a countinuous surface
-densitymap <- density(bei)
+# Passing from points to a continuous surface:
+densitymap <- density(bei) # apparently, the density() function "computes kernel density estimates". I don't know what this means
 plot(densitymap)
 points(bei, cex=.2)
 
-cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100)
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(100) # description: "These functions return functions that interpolate a set of given colors to create new color palettes (like topo.colors) and color ramps, functions that map the interval [0, 1] to colors (like grey)." Basically, it creates the color palette
+plot(densitymap, col = cl) # plots densitymap with the palette we just created
+
+cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4) # changes the number of intermediate colors between each of the colors in the array
 plot(densitymap, col=cl)
 
-cl <- colorRampPalette(c("black", "red", "orange", "yellow"))(4)
-plot(densitymap, col=cl)
-
-clnew <- colorRampPalette(c("dark blue", "blue", "light blue"))(100)
+clnew <- colorRampPalette(c("dark blue", "blue", "light blue"))(100) # changes the colors in the palette
 plot(densitymap, col=clnew)
 
-plot(bei.extra)
+elev <- elevation # renaming "elevation" because we're lazy ^^
 
-elev <- bei.extra[[1]] # bei.extra$elev
-plot(elev)
-
-# multiframe
-par(mfrow=c(1,2))
+# Building a multiframe:
+par(mfrow = c(1,2))
 plot(densitymap)
 plot(elev)
 
