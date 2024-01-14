29ce728e13375f2be63248cf34a4c0e8efd3eaa5
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-14 23:39:05 +0100

    Update 03 communities multivariate analysis.R

--- 03 communities multivariate analysis.R
+++ 03 communities multivariate analysis.R
@@ -1,79 +1,25 @@
-# Why populations disperse over the landscape in a certain manner?
+library(vegan)
 
-library(sdm)
-library(terra)
+data(dune)
+head(dune)
 
-file <- system.file("external/species.shp", package="sdm")
+ord <- decorana(dune)
 
-rana <- vect(file)
-rana$Occurrence
+ldc1 =  3.7004 
+ldc2 =  3.1166 
+ldc3 = 1.30055
+ldc4 = 1.47888
 
-plot(rana)
+total = ldc1 + ldc2 + ldc3 + ldc4
 
-# Selecting presences
-pres <- rana[rana$Occurrence==1,]
-plot(pres)
+pldc1 = ldc1 * 100 / total
+pldc2 = ldc2 * 100 / total
+pldc3 = ldc3 * 100 / total
+pldc4 = ldc4 * 100 / total
 
-# exercise: select absence and call them abse
-abse <- rana[rana$Occurrence==0,]
-plot(abse)
+pldc1
+pldc2
 
-# exrecise: plot presences and absences, one beside the other
-par(mfrow=c(1,2))
-plot(pres)
-plot(abse)
+pldc1 + pldc2
 
-# your new friend in case of graphical nulling:
-dev.off()
-
-# exercise: plot pres and abse altogether with two different colours
-plot(pres, col="dark blue")
-points(abse, col="light blue")
-
-# predictors: environmental variables
-# file <- system.file("external/species.shp", package="sdm")
-# rana <- vect(file)
-
-# elevation predictor
-elev <- system.file("external/elevation.asc", package="sdm") 
-elevmap <- rast(elev) # from terra package
-plot(elevmap)
-points(pres, cex=.5)
-
-# temperature predictor
-temp <- system.file("external/temperature.asc", package="sdm") 
-tempmap <- rast(temp) # from terra package
-plot(tempmap)
-points(pres, cex=.5)
-
-# exrcise: do the same with vegetation cover
-vege <- system.file("external/vegetation.asc", package="sdm") 
-vegemap <- rast(vege) # from terra package
-plot(vegemap)
-points(pres, cex=.5)
-
-# exrcise: do the same with vegetation cover
-prec <- system.file("external/precipitation.asc", package="sdm") 
-precmap <- rast(prec) # from terra package
-plot(precmap)
-points(pres, cex=.5)
-
-# final multiframe
-
-par(mfrow=c(2,2))
-
-# elev
-plot(elevmap)
-points(pres, cex=.5)
-
-# temp
-plot(tempmap)
-points(pres, cex=.5)
-
-# vege
-plot(vegemap)
-points(pres, cex=.5)
-
-# prec
-plot(precmap)
-points(pres, cex=.5)
+plot(ord) 
