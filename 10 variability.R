8acc5348efe93be03d7405e1e882d8c5a9895b2f
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-15 18:26:20 +0100

    Update 10 variability.R

--- 10 variability.R
+++ 10 variability.R
@@ -1,4 +1,4 @@
-# measurement of RS based variability
+# We will learn how to measure RS-based variability
 
 library(imageRy)
 library(terra)
@@ -12,29 +12,32 @@ sent <- im.import("sentinel.png")
 # band 2 = red
 # band 3 = green
 
-im.plotRGB(sent, r=1, g=2, b=3)
-im.plotRGB(sent, r=2, g=1, b=3)
+im.plotRGB(sent, r = 1, g = 2, b = 3)
+im.plotRGB(sent, r = 2, g = 1, b = 3)
 
 nir <- sent[[1]]
 plot(nir)
 
-# moving window
-# focal
-sd3 <- focal(nir, matrix(1/9, 3, 3), fun=sd)
+### Using a moving window and the focal function ###
+
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun = sd)
 plot(sd3)
 
 viridisc <- colorRampPalette(viridis(7))(255)
-plot(sd3, col=viridisc)
+plot(sd3, col = viridisc)
 
-# Exercise: calculate variability in a 7x7 pixels moving window
-sd7 <- focal(nir, matrix(1/49, 7, 7), fun=sd)
-plot(sd7, col=viridisc)
+### Exercise: calculate variability in a 7x7 pixels moving window ###
 
-# Exercise 2: plot via par(mfrow()) the 3x3 and the 7x7 standard deviation
-par(mfrow=c(1,2))
-plot(sd3, col=viridisc)
-plot(sd7, col=viridisc)
+sd7 <- focal(nir, matrix(1/49, 7, 7), fun = sd)
+plot(sd7, col = viridisc)
 
-# original image plus the 7x7 sd
-im.plotRGB(sent, r=2, g=1, b=3)
-plot(sd7, col=viridisc)
+### Exercise : plot via par(mfrow = c()) the 3x3 and the 7x7 standard deviation ###
+
+par(mfrow = c(1,2))
+plot(sd3, col = viridisc)
+plot(sd7, col = viridisc)
+
+#### Plotting the original image plus the 7x7 sd ###
+
+im.plotRGB(sent, r = 2, g = 1, b = 3)
+plot(sd7, col = viridisc)
