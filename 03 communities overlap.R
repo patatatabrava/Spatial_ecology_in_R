0757d9268c26cba52179fde3f56b7235b3a1c552
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-15 14:47:02 +0100

    Update 03 communities overlap.R

--- 03 communities overlap.R
+++ 03 communities overlap.R
@@ -1,22 +1,25 @@
-# relation among species in time
+# We will study the relation among species in time. For this, we will use the overlap library. 
+# This library contains functions to estimate the degree of overlap between the activity patterns of two species, tigers and macaques, based on camera trap data
+# /!\ It is not on CRAN
 
 library(overlap)
 
 data(kerinci)
-summary(kerinci)
+summary(kerinci) # summary() gives a summary of the object it takes as first argument, here the dataset kerinci
 
-kerinci$timeRad <- kerinci$Time * 2 * pi
+kerinci$timeRad <- kerinci$Time*2*pi # selects the time variable, puts it in radians and stores it in a new variable added to the data frame kerinci, called timeRad
+# But why would you express a time in radians?
 
-tiger <- kerinci[kerinci$Sps=="tiger",]
+tiger <- kerinci[kerinci$Sps == "tiger",] # selects the elements in kerinci for which the Sps (species) variable is tiger
 
-timetig <- tiger$timeRad
-densityPlot(timetig, rug=TRUE)
+timetig <- tiger$timeRad # selects the time variable in the tiger data and stores it in a variable called timetig
+densityPlot(timetig, rug = TRUE) # what does "rug = TRUE" mean?
 
-# exercise: select only the data on macaque individuals
-macaque <- kerinci[kerinci$Sps=="macaque",]
+### Exercise: select only the data on macaque individuals and plot it ###
+
+macaque <- kerinci[kerinci$Sps == "macaque",]
 head(macaque)
-
 timemac <- macaque$timeRad
-densityPlot(timemac, rug=TRUE)
+densityPlot(timemac, rug = TRUE)
 
-overlapPlot(timetig, timemac)
+overlapPlot(timetig, timemac) # shows the overlap between the tiger and macaque activity
