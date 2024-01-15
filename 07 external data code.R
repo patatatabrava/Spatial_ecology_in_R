5d0f26ce385fb70bbadda20d17a55106a6592925
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-15 17:55:08 +0100

    Update 07 external data code.R

--- 07 external data code.R
+++ 07 external data code.R
@@ -1,39 +1,41 @@
-# External data
+# We will learn how to download external data
 
 library(terra)
 
-# set the working directory based on your path:
-# setwd("youtpath")
+# How to set the working directory based on your path:
+# setwd("yourtpath")
 # W***** users: C:\\path\Downloads -> C://path/Downloads
-# My own:
+# The professor's:
 setwd("~/Downloads")
 naja <- rast("najafiraq_etm_2003140_lrg.jpg")  # like in im.import()
 
-plotRGB(naja, r=1, g=2, b=3) # im-plotRGB
+plotRGB(naja, r = 1, g = 2, b = 3) # im-plotRGB
 
-# Exercise: Download the second image from the same site and import it in R
+### Exercise: Download the second image from the same site and import it in R ###
+
 najaaug <- rast("najafiraq_oli_2023219_lrg.jpg")
-plotRGB(najaaug, r=1, g=2, b=3)
+plotRGB(najaaug, r = 1, g = 2, b = 3)
 
-par(mfrow=c(2,1))
-plotRGB(naja, r=1, g=2, b=3) # im-plotRGB
-plotRGB(najaaug, r=1, g=2, b=3)
+par(mfrow = c(2,1))
+plotRGB(naja, r = 1, g = 2, b = 3) # im.plotRGB
+plotRGB(najaaug, r = 1, g = 2, b = 3)
 
-# multitemporal change detection
+### Multitemporal change detection ###
+
 najadif = naja[[1]] - najaaug[[1]] 
 cl <- colorRampPalette(c("brown", "grey", "orange")) (100)
-plot(najadif, col=cl)
+plot(najadif, col = cl)
 
-# Download your own preferred image:
+### Downloading and plotting in RGB space your own preferred image ###
+
 typhoon <- rast("mawar_vir2_2023144_lrg.jpg")
 
-plotRGB(typhoon, r=1, g=2, b=3)
-plotRGB(typhoon, r=2, g=1, b=3)
-plotRGB(typhoon, r=3, g=2, b=1)
+plotRGB(typhoon, r = 1, g = 2, b = 3)
+plotRGB(typhoon, r = 2, g = 1, b = 3)
+plotRGB(typhoon, r = 3, g = 2, b = 1)
 
 
 # The Mato Grosso image can be downloaded directly from EO-NASA:
-
 mato <- rast("matogrosso_l5_1992219_lrg.jpg")
-plotRGB(mato, r=1, g=2, b=3) 
-plotRGB(mato, r=2, g=1, b=3) 
+plotRGB(mato, r = 1, g = 2, b = 3) 
+plotRGB(mato, r = 2, g = 1, b = 3) 
