8e24e102beb6445547f2d798810eb7d92a99de24
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-29 14:51:00 +0100

    Update 01 beginning.R

--- 01 beginning.R
+++ 01 beginning.R
@@ -1,5 +1,7 @@
-# R can be used to make calculations. You don't have to use "print" to visualize the output of a function, it automatically appears in the console, after a quote of the corresponding line of code (at least in R studio)
+### Using R as a calculator ###
+
 2 + 3
+# You don't have to use "print" to visualize the output of a function. It automatically appears in the console, right after the line of code in which you called the function (at least in R studio)
 
 # This is the way you assign a value to an object:
 zima <- 2 + 3
@@ -13,14 +15,16 @@ final
 
 final^2 # this is the syntax for taking exponents
 
+### Making arrays and plots ###
+
 # The c() function is used to make arrays. It forces all its arguments into a common type and its output is of the same type. "c" stands for "concatenate"
 # Examples:
 microplastics <- c(10, 20, 30, 50, 70) 
 people <- c(100, 500, 600, 1000, 2000)
 
-plot(people, microplastics) # shows the number of microplastics as a function of the number of people, without labelling the axes. Notice you don't need to import a library to make plots, unlike in Python
+plot(people, microplastics) # shows the number of microplastics as a function of the number of people, without labelling the axes. You don't need to import a library to make plots, unlike in Python
 
-plot(people, microplastics, xlab = "number of people", ylab = "microplastics") # puts labels on the axes
+plot(people, microplastics, xlab = "number of people", ylab = "microplastics") # makes the same plot with labels on the axes
 
 plot(people, microplastics, pch = 19) # "pch = 19" selects point shape 19 (see tables on the interwebz)
 # Link to such tables: https://www.google.com/search?client=ubuntu-sn&hs=yV6&sca_esv=570352775&channel=fs&sxsrf=AM9HkKknoSOcu32qjoErsqX4O1ILBOJX4w:1696347741672&q=point+symbols+in+R&tbm=isch&source=lnms&sa=X&ved=2ahUKEwia9brkm9qBAxVrQvEDHbEYDuMQ0pQJegQIChAB&biw=1760&bih=887&dpr=1.09#imgrc=lUw3nrgRKV8ynM
