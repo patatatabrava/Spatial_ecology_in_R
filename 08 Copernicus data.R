0995f5c17e2192e0ba4e853ad29bd08780fe93d2
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-29 18:46:24 +0100

    Update 08 Copernicus data.R

--- 08 Copernicus data.R
+++ 08 Copernicus data.R
@@ -10,14 +10,16 @@ setwd("~/Downloads") # in Windows '\' means '/'
 soilm2023 <- rast("c_gls_SSM1km_202311250000_CEURO_S1CSAR_V1.2.1.nc") # the data concerns soil moisture in November 2023
 plot(soilm2023)
 
-# there are two elements, let's use the first one!
+# There are two elements, let's use the first one!
 plot(soilm2023[[1]])
 
 cl <- colorRampPalette(c("red", "orange", "yellow")) (100)
 plot(soilm2023[[1]], col = cl)
 
-ext <- c(22, 26, 55, 57) # minlong, maxlong, minlat, maxlat
-soilm2023c <- crop(soilm2023, ext)
+### Cropping the dataset ###
+
+ext <- c(22, 26, 55, 57) # the parameters are in the following order: minimal longitude, maximal longitude, minimal latitude, maximal latitude
+soilm2023c <- crop(soilm2023, ext) # crops the dataset to the extent specified by the ext array and stores the cropped dataset in a new variable
 
 plot(soilm2023c[[1]], col = cl)
 
