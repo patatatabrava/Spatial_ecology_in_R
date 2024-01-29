e4f4c29cbe3b07f7575841f8a69588223d1d8f61
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-29 15:48:11 +0100

    Update 00 final script.R

--- 00 final script.R
+++ 00 final script.R
@@ -248,30 +248,46 @@ plot(ord)
 
 #--------------------
 
-# 03.2 Comjmunity overlap
+# 03.2 Community overlap
 
-# relation among species in time
+# We will use a library for community ecology called vegan. Link to its documentation: https://cran.r-project.org/web/packages/vegan/
 
-library(overlap)
+library(vegan)
 
-data(kerinci)
-summary(kerinci)
+data(dune) # loads the dune dataset. It concerns vegetation and environment in Dutch dune meadows (see documentation). The data() function is the standard way of loading data
+head(dune) # returns the start of the dataset
 
-kerinci$timeRad <- kerinci$Time * 2 * pi
+ord <- decorana(dune) # description of the decorana() function: "Performs detrended correspondence analysis (DCA) and basic reciprocal averaging or orthogonal correspondence analysis." 
+# DCA is a multivariate statistical method used in ecology to analyze and visualize species composition data
 
-tiger <- kerinci[kerinci$Sps=="tiger",]
+#### Storing the lengths of the first 4 axes of the DCA in 4 variables ###
 
-timetig <- tiger$timeRad
-densityPlot(timetig, rug=TRUE)
+ldc1 = 3.7004
+ldc2 = 3.1166 
+ldc3 = 1.30055
+ldc4 = 1.47888
 
-# exercise: select only the data on macaque individuals
-macaque <- kerinci[kerinci$Sps=="macaque",]
-head(macaque)
+# We can use "=" instead of "<-" because we're manipulating numbers
 
-timemac <- macaque$timeRad
-densityPlot(timemac, rug=TRUE)
+### Calculating the contribution of each axis to the total axis length (as a percentage) ###
 
-overlapPlot(timetig, timemac)
+total = ldc1 + ldc2 + ldc3 + ldc4
+
+pldc1 = ldc1 * 100 / total
+pldc2 = ldc2 * 100 / total
+pldc3 = ldc3 * 100 / total
+pldc4 = ldc4 * 100 / total
+
+### Displaying two of the contributions, their sum and the DCA of ord ###
+
+pldc1
+pldc2
+
+pldc1 + pldc2
+
+# Together, the first two axes contribute around 71.04% of the total variation in the data. The first axis contributes the most to it (38.56%)
+
+plot(ord) 
 
 #--------------------
 
