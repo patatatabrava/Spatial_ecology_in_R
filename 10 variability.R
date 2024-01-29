cde718fc645fc937fee6371adc55dcf21d384cab
Cloé Rouch <147079499+patatatabrava@users.noreply.github.com>
2024-01-29 19:48:27 +0100

    Update 10 variability.R

--- 10 variability.R
+++ 10 variability.R
@@ -20,7 +20,13 @@ plot(nir)
 
 ### Using a moving window and the focal function ###
 
-sd3 <- focal(nir, matrix(1/9, 3, 3), fun = sd)
+sd3 <- focal(nir, matrix(1/9, 3, 3), fun = sd) # the focal() function takes an image (a raster) as first argument, 
+# a matrix of weights (the moving window) as second argument and a function as an optional third argument. 
+# Here, the function calculates the standard deviation, which is what "sd" stands for.
+# The first value in the matrix function's parameters is the weight of each point of the moving window.
+# The next two are the number of columns and rows respectively.
+# Description of the focal() function: 
+# "calculate focal ("moving window") values for the neighborhood of focal cells using a matrix of weights, perhaps in combination with a function"
 plot(sd3)
 
 viridisc <- colorRampPalette(viridis(7))(255)
